e3607d772302384ed2e59b9540ae31ba7268e351
Cristina Stoian <cristina.stoian@yahoo.com>
2024-09-30 11:29:33 +0200

    Fix: Improve responsiveness for FABMenu and Contact page UI.

--- src/components/FABMenu/FABMenu.jsx
+++ src/components/FABMenu/FABMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaPlus } from "react-icons/fa";
 import css from "./FABMenu.module.css";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
@@ -9,7 +9,30 @@ import GoToGithub from "../GoToGithub/GoToGithub"; // The GoToGithub component
 const FABMenu = ({ onToggleExpand }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const fabRef = useRef(null); 
 
+
+
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (fabRef.current && !fabRef.current.contains(event.target)) {
+        setIsExpanded(false); // Close the FABMenu when clicking outside
+        onToggleExpand && onToggleExpand(false); // Notify the parent
+      }
+    };
+
+    if (isExpanded) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    // Clean up the event listener
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isExpanded, onToggleExpand]);
 
  
 
@@ -20,7 +43,7 @@ const FABMenu = ({ onToggleExpand }) => {
   };
 
   return (
-    <div className={css.fabContainer}>
+    <div className={css.fabContainer} ref={fabRef}>
       {/* Floating Action Button with FaPlus as children */}
       <GoToGithub onClick={handleFabToggle}>
         <FaPlus />
