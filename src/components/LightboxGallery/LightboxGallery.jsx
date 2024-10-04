85568459b86d3e3b0a0cd873322da04d03fe4530
Cristina Stoian <cristina.stoian@yahoo.com>
2024-10-04 12:27:51 +0200

    Full Screen modifiy.

--- src/components/LightboxGallery/LightboxGallery.jsx
+++ src/components/LightboxGallery/LightboxGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import css from "./LightboxGallery.module.css";
@@ -7,6 +7,7 @@ import Modal from "../Modal/Modal";
 export const LightboxGallery = ({ images }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(0);
+  const galleryRef = useRef(null);
 
   const galleryImages = images.map((imgSrc) => ({
     original: imgSrc,
@@ -20,6 +21,16 @@ export const LightboxGallery = ({ images }) => {
     setIsOpen(true);
   };
 
+  const handleFullScreen = () => {
+    if (galleryRef.current) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        galleryRef.current.requestFullScreen();
+      }
+    }
+  };
+
   return (
     <div>
       <div className={css.galleryContainer}>
@@ -27,7 +38,7 @@ export const LightboxGallery = ({ images }) => {
           <img
             key={index}
             src={image}
-            alt={`Gallery Image ${index + 1}`}
+            alt={`Gallery ${index + 1}`}
             className={`${css.thumbnail} cursor-pointer hover:opacity-75 rounded-lg shadow-lg transition-transform duration-300 transform hover:scale-105`}
             onClick={() => handleImageClick(index)}
           />
@@ -35,17 +46,20 @@ export const LightboxGallery = ({ images }) => {
       </div>
 
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <ImageGallery
-          items={galleryImages}
-          startIndex={photoIndex}
-          showThumbnails={false}
-          onSlide={(currentIndex) => setPhotoIndex(currentIndex)}
-          infinite={true}
-          showPlayButton={false}
-          showFullscreenButton={true}
-          showIndex={false}
-          showBullets={true}
-        />
+        <div ref={galleryRef} className={css.fullscreenWrapper}>
+          <ImageGallery
+            items={galleryImages}
+            startIndex={photoIndex}
+            showThumbnails={false}
+            onSlide={(currentIndex) => setPhotoIndex(currentIndex)}
+            infinite={true}
+            showPlayButton={false}
+            showFullscreenButton={true}
+            onFullScreen={handleFullScreen}
+            showIndex={false}
+            showBullets={true}
+          />
+        </div>
       </Modal>
     </div>
   );
