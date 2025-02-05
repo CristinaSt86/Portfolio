3e87972e5f4909fa82c840d2ded8e720c7edb266
Cristina Stoian <cristina.stoian@yahoo.com>
2025-02-05 13:22:05 +0100

    Add my name instead of CSDevelopment logo in the Header and footer.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,43 @@
+// import React from "react";
+// import css from "./Header.module.css";
+// //import BackButton from "../BackButton/BackButton";
+// import Navigation from "../Navigation/Navigation";
+// import { Link } from "react-router-dom";
+// import logo from "../../images/logo.svg";
+
+
+// const Header = () => {
+//   return (
+//     <div className={css.headerContainer}>
+//       <div className={css.logoCont}>
+//         {/* <BackButton /> */}
+//         <Link to="/">
+//           <img src={logo} alt="logo" className={css.logo} />
+//         </Link>
+//       </div>
+//       <div className={css.langThemeContainer}>
+//         <Navigation />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
+
 import React from "react";
 import css from "./Header.module.css";
 //import BackButton from "../BackButton/BackButton";
 import Navigation from "../Navigation/Navigation";
 import { Link } from "react-router-dom";
-import logo from "../../images/logo.svg";
-
 
 const Header = () => {
   return (
     <div className={css.headerContainer}>
       <div className={css.logoCont}>
         {/* <BackButton /> */}
-        <Link to="/">
-          <img src={logo} alt="logo" className={css.logo} />
+        <Link to="/" className={css.logo}>
+          Cristina Stoian
         </Link>
       </div>
       <div className={css.langThemeContainer}>
