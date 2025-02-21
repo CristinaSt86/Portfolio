19c3049b5a237c4fba8801618b81596453654a93
Cristina Stoian <cristina.stoian@yahoo.com>
2025-02-21 13:32:13 +0100

    Add shadow to Button. Add padding to body.

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
           <Suspense fallback={<Loader />}>
             <ThemeWrapper>
               {/* <Shapes /> */}
+              
               <Header />
               <RoutesConfig />
               <BackToTop />
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import css from "./ProjectsPage.module.css";
 import WeatherMini from "../../components/WeatherMini/WeatherMini";
 import { useTranslation } from "react-i18next";
